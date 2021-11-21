4003a6bff99c744599a1164e36246b860970920a
retroxz <zzxee666@gmail.com>
2021-11-21 14:25:02 +0800

    :construction: vue2.x到3.x的语法修改

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+        // 函数名和参数列表之间的空格
+        'space-before-function-paren': ['error', {
+            // 匿名函数必须要空格
+            'anonymous': 'always',
+            // 具名函数必须不要空格
+            'named': 'never',
+        }],
+        // 允许未使用的定义
+        'no-unused-vars': 'off'
+    }
+}
