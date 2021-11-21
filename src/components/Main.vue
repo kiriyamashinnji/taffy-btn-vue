8aed0dd3474373d12a9aed7c0d6108645a9ca731
huiwy <huiwy@shanghaitech.edu.cn>
2021-11-21 14:31:48 +0800

    🥵 init main and navbars

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    <div v-for="btns in btnOverview" :key="btns">
+      <TaffyBtnGroup :btns="btns" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TaffyBtnGroup from "./TaffyBtnGroup.vue";
+
+export default {
+  name: "Main",
+  components: {
+    TaffyBtnGroup,
+  },
+  data: () => {
+    return {};
+  },
+
+  props: {
+    btnOverview: Object,
+    sideBarWidth: String,
+  },
+};
+</script>
+
+<style>
+.main {
+  margin-left: v-bind(sideBarWidth);
+}
+</style>
